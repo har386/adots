d997b6e8dc36914a7039d53cb54b9a32fa440fc7
har386 <har386@zoho.com>
2018-10-22 20:33:50 +0300

    ddd

--- strapon.sh
+++ strapon.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-ln -sv ~/adots/.gitconfig ~
 
-#sudo pacman --noconfirm -Syu
-#sudo pacman --noconfirm -S xorg xorg-drivers xorg-xinit xterm openbox firefox termite tmux vim fish noto-fonts compton ttf-hack termite-terminfo keepassxc htop tree
+sudo pacman --noconfirm -Syu
+sudo pacman --noconfirm -S xorg xorg-drivers xorg-xinit xterm openbox firefox termite tmux vim fish noto-fonts compton ttf-hack termite-terminfo keepassxc htop tree arandr
 
 mkdir ~/.config
 mkdir ~/.config/fontconfig
@@ -12,8 +11,10 @@ cp -R /etc/xdg/openbox/ ~/.config/
 cp -R /etc/xdg/termite/ ~/.config/
 
 rm ~/.config/openbox/menu.xml 
+
+ln -sv ~/adots/.gitconfig ~
+ln -sv ~/adots/.xinitrc ~
+ln -sv ~/adots/.tmux.conf ~
 ln -sv ~/adots/.config/openbox/menu.xml ~/.config/openbox
 ln -sv ~/adots/.config/fontconfig/fonts.conf ~/.config/fontconfig
-
-
 
